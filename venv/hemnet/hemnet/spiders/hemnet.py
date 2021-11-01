968043ea1bd671c1923542a566e72266f9c6e03f
alsakif <alsakif1.618@gmail.com>
2021-11-01 09:35:23 +0600

    Finished Alhamdulillah

--- venv/hemnet/hemnet/spiders/hemnet.py
+++ venv/hemnet/hemnet/spiders/hemnet.py
@@ -1,8 +1,16 @@
 import scrapy
+import json
+from pydispatch import dispatcher
+from scrapy import signals
 
 class HemnetSpider(scrapy.Spider):
     name = "hemnet"
     start_urls = ['https://www.hemnet.se/bostader?location_ids%5B%5D=17755&item_types%5B%5D=bostadsratt']
+    counter = 0
+    results={}
+
+    def __init__(self):
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
 
     def parse(self, response):
         #Text Mining
@@ -23,8 +31,31 @@ class HemnetSpider(scrapy.Spider):
         #Cleaning data
         price = price.replace("kr", "") # Cleaning currency sign
         price = price.replace(u"\xa0", "") #Cleaning White Spaces
-        
+
+        attrData = {}
         for attr in response.css("div.property-attributes > div.property-attributes-table > dl.property-attributes-table__area > div.property-attributes-table__row"):
-            attrLabel = attr.css("dt.property-attributes-table__label").get() #Text Mining
+            attrLabel = attr.css("dt.property-attributes-table__label::text").get() #Text Mining
+            if attrLabel is not None:
+                attrLabel = attrLabel.replace(u"\n", "")
+                attrLabel = attrLabel.replace(u"\t", "")
+                attrLabel = attrLabel.strip()
+
+            attrValue = attr.css("dd.property-attributes-table__value::text").get() #Text Mining
+            if attrValue is not None:
+                attrValue = attrValue.replace(u"\n", "")
+                attrValue = attrValue.replace(u"\t", "")
+                attrValue = attrValue.strip()
             
-            attrValue = attr.css("dd.property-attributes-table__value").get() #Text Mining
+            if attrLabel is not None:
+                attrData[attrLabel] = attrValue
+        
+        self.results[self.counter] = {
+            "streetName" : streetName,
+            "price" : price,
+            "attr" : attrData
+        }
+        self.counter+=1 
+
+    def spider_closed(self,spider):
+        with open('results.json', 'w') as fp:
+            json.dump(self.results,fp)         
